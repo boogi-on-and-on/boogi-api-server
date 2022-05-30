664053c0d797281c8e5b705ec56ff45ddf56e182
yjlee0235 <yjlee0235@naver.com>
2022-05-31 01:02:29 +0900

    refactor: 댓글 depth 확인 부분의 exception수정

--- src/main/java/boogi/apiserver/domain/comment/application/CommentValidationService.java
+++ src/main/java/boogi/apiserver/domain/comment/application/CommentValidationService.java
@@ -4,6 +4,7 @@ import boogi.apiserver.domain.comment.dao.CommentRepository;
 import boogi.apiserver.domain.comment.domain.Comment;
 import boogi.apiserver.domain.comment.exception.CommentMaxDepthOverException;
 import boogi.apiserver.global.error.exception.EntityNotFoundException;
+import boogi.apiserver.global.error.exception.ErrorInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,7 +22,9 @@ public class CommentValidationService {
         }
 
         Comment findParentComment = commentRepository.findById(parentCommentId)
-                .orElseThrow(EntityNotFoundException::new);
+                .orElseThrow(() -> {
+                    throw new EntityNotFoundException("해당 댓글의 부모 댓글을 찾을 수 없습니다", ErrorInfo.NOT_FOUND);
+                });
 
         if (findParentComment.getParent() != null) {
             throw new CommentMaxDepthOverException();
