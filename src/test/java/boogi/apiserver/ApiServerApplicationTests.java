5e20ba7c0de829872f489c042c5f3f901935bd37
yjlee0235 <yjlee0235@naver.com>
2022-08-19 17:38:48 +0900

    fix: applicationTest 비활성화

--- src/test/java/boogi/apiserver/ApiServerApplicationTests.java
+++ src/test/java/boogi/apiserver/ApiServerApplicationTests.java
@@ -3,11 +3,11 @@ package boogi.apiserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ApiServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
