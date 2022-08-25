1dadedd6f3c6ac770de1aca2bcfee354db1e62de
yjlee0235 <yjlee0235@naver.com>
2022-08-26 00:39:54 +0900

    fix: pagination 객체 Page -> Slice로 변경

--- src/main/java/boogi/apiserver/global/dto/PaginationDto.java
+++ src/main/java/boogi/apiserver/global/dto/PaginationDto.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
 
 @Data
 @AllArgsConstructor
@@ -14,12 +14,12 @@ public class PaginationDto {
     protected int nextPage;
     protected boolean hasNext;
 
-    public PaginationDto(Page page) {
+    public PaginationDto(Slice page) {
         this.nextPage = page.getNumber() + 1;
         this.hasNext = page.hasNext();
     }
 
-    public static PaginationDto of(Page page) {
+    public static PaginationDto of(Slice page) {
         return new PaginationDto(page);
     }
 }
