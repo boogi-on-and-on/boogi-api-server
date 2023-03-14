5bcb985645f830f11da5b6fe6202f9afce16cc2a
yjlee0235 <yjlee0235@naver.com>
2023-03-14 17:56:59 +0900

    Test: PostMedia, Report Service 테스트 수정 및 추가

--- src/main/java/boogi/apiserver/domain/report/exception/InvalidReportTargetException.java
+++ src/main/java/boogi/apiserver/domain/report/exception/InvalidReportTargetException.java
@@ -2,10 +2,10 @@ package boogi.apiserver.domain.report.exception;
 
 import boogi.apiserver.global.error.exception.InvalidValueException;
 
-public class InvalidReportException extends InvalidValueException {
+public class InvalidReportTargetException extends InvalidValueException {
     private static final String MESSAGE = "잘못된 신고 대상입니다.";
 
-    public InvalidReportException() {
+    public InvalidReportTargetException() {
         super(MESSAGE);
     }
 }
