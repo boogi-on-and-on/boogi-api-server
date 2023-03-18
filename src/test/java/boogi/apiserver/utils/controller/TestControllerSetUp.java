c6d87e6cb4ec7f1ba6868a561a2ae56a704039fe
yjlee0235 <yjlee0235@naver.com>
2023-03-18 16:30:29 +0900

    fix: document 2번 작동되는 버그 수정

--- src/test/java/boogi/apiserver/utils/controller/TestControllerSetUp.java
+++ src/test/java/boogi/apiserver/utils/controller/TestControllerSetUp.java
@@ -10,18 +10,13 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @ExtendWith(RestDocumentationExtension.class)
 public class TestControllerSetUp {
-
-    protected ObjectMapper mapper;
-
-    protected final String DEFAULT_REST_DOCS_IDENTIFIER = "{class-name}/{method-name}";
+    protected ObjectMapper mapper = new ObjectMapper();
 
     protected MockMvc mvc;
 
@@ -30,15 +25,11 @@ public class TestControllerSetUp {
         this.mvc =
                 MockMvcBuilders.webAppContextSetup(context)
                         .addFilter(new CharacterEncodingFilter("UTF-8", true))
-                        .apply(documentationConfiguration(provider))
+                        .apply(documentationConfiguration(provider)
+                                .operationPreprocessors()
+                                .withRequestDefaults(prettyPrint())
+                                .withResponseDefaults(prettyPrint()))
                         .alwaysDo(print())
-                        .alwaysDo(document(DEFAULT_REST_DOCS_IDENTIFIER,
-                                        preprocessRequest(prettyPrint()),
-                                        preprocessResponse(prettyPrint())
-                                )
-                        )
                         .build();
-
-        this.mapper = new ObjectMapper();
     }
 }
