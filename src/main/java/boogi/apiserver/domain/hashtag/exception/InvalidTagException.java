867bdd94b9315caf5191c6fccc623bcd93a053a7
sdcodebase <sdcodebase@gmail.com>
2023-04-28 11:10:20 +0900

    Fix: 해시태그의 에러 메시지 변경

--- src/main/java/boogi/apiserver/domain/hashtag/exception/InvalidTagException.java
+++ src/main/java/boogi/apiserver/domain/hashtag/exception/InvalidTagException.java
@@ -5,7 +5,7 @@ import boogi.apiserver.global.error.exception.InvalidValueException;
 
 public class InvalidTagException extends InvalidValueException {
 
-    private static final String MESSAGE = "태그는 " + Tag.MIN_LENGTH + " ~ " + Tag.MAX_LENGTH + " 길이의 영어나 한글이어야 합니다.";
+    private static final String MESSAGE = "태그는 공백이 없으며, " + Tag.MIN_LENGTH + " ~ " + Tag.MAX_LENGTH + " 길이의 영어나 한글이어야 합니다.";
 
     public InvalidTagException() {
         super(MESSAGE);
