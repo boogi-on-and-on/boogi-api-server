086337c288df56131657ca3a9bf8eb4a72e45a4a
yjlee0235 <yjlee0235@naver.com>
2023-05-18 09:28:54 +0900

    Test: Like ServiceTest에 Fixture 적용

--- src/test/java/boogi/apiserver/domain/like/application/LikeCommandServiceTest.java
+++ src/test/java/boogi/apiserver/domain/like/application/LikeCommandServiceTest.java
@@ -1,19 +1,19 @@
 package boogi.apiserver.domain.like.application;
 
-import boogi.apiserver.builder.*;
-import boogi.apiserver.domain.comment.repository.CommentRepository;
 import boogi.apiserver.domain.comment.domain.Comment;
+import boogi.apiserver.domain.comment.repository.CommentRepository;
 import boogi.apiserver.domain.community.community.domain.Community;
-import boogi.apiserver.domain.like.repository.LikeRepository;
 import boogi.apiserver.domain.like.domain.Like;
 import boogi.apiserver.domain.like.exception.AlreadyDoCommentLikeException;
 import boogi.apiserver.domain.like.exception.AlreadyDoPostLikeException;
 import boogi.apiserver.domain.like.exception.UnmatchedLikeUserException;
+import boogi.apiserver.domain.like.repository.LikeRepository;
 import boogi.apiserver.domain.member.application.MemberQueryService;
 import boogi.apiserver.domain.member.domain.Member;
-import boogi.apiserver.domain.post.post.repository.PostRepository;
 import boogi.apiserver.domain.post.post.domain.Post;
+import boogi.apiserver.domain.post.post.repository.PostRepository;
 import boogi.apiserver.domain.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -23,7 +23,15 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.List;
+
+import static boogi.apiserver.utils.fixture.CommentFixture.COMMENT1;
+import static boogi.apiserver.utils.fixture.CommunityFixture.POCS;
+import static boogi.apiserver.utils.fixture.MemberFixture.SUNDO_POCS;
+import static boogi.apiserver.utils.fixture.PostFixture.POST1;
+import static boogi.apiserver.utils.fixture.UserFixture.SUNDO;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
@@ -54,6 +62,21 @@ class LikeCommandServiceTest {
     @Captor
     ArgumentCaptor<Like> likeCaptor;
 
+    private User user;
+    private Community community;
+    private Member member;
+    private Post post;
+    private Comment comment;
+
+    @BeforeEach
+    void init() {
+        user = SUNDO.toUser(1L);
+        community = POCS.toCommunity(2L, List.of());
+        member = SUNDO_POCS.toMember(3L, user, community);
+        post = POST1.toPost(4L, member, community, List.of(), List.of());
+        comment = COMMENT1.toComment(5L, post, member, null);
+    }
+
     @Nested
     @DisplayName("글에 좋아요할 시")
     class DoLikeAtPostTest {
@@ -61,49 +84,26 @@ class LikeCommandServiceTest {
         @Test
         @DisplayName("해당 글의 좋아요 수가 1 증가하고, 좋아요가 생성된다.")
         void doLikeAtPostSuccess() {
-            final Community community = TestCommunity.builder().id(1L).build();
-
-            final Post post = TestPost.builder()
-                    .id(1L)
-                    .community(community)
-                    .likeCount(0)
-                    .build();
-
-            final Member member = TestMember.builder().id(2L).build();
-
-            given(postRepository.findPostById(anyLong()))
-                    .willReturn(post);
-            given(memberQueryService.getMember(anyLong(), anyLong()))
-                    .willReturn(member);
+            given(postRepository.findPostById(anyLong())).willReturn(post);
+            given(memberQueryService.getMember(anyLong(), anyLong())).willReturn(member);
             given(likeRepository.existsLikeByPostIdAndMemberId(anyLong(), anyLong()))
                     .willReturn(false);
 
             likeCommandService.doPostLike(post.getId(), 3L);
 
             verify(likeRepository, times(1)).save(likeCaptor.capture());
-            Like newLike = likeCaptor.getValue();
 
-            assertThat(newLike.getPost().getId()).isEqualTo(1L);
-            assertThat(newLike.getMember().getId()).isEqualTo(2L);
-            assertThat(newLike.getPost().getLikeCount()).isEqualTo(1);
+            Like newLike = likeCaptor.getValue();
+            assertThat(newLike.getPost().getId()).isEqualTo(4L);
+            assertThat(newLike.getMember().getId()).isEqualTo(3L);
+            assertThat(newLike.getPost().getLikeCount()).isEqualTo(2);
         }
 
         @Test
         @DisplayName("해당 멤버가 해당 글에 이미 좋아요를 누른 경우 AlreadyDoPostLikeException 발생한다.")
         void alreadyDoLikeFail() {
-            final Community community = TestCommunity.builder().id(1L).build();
-
-            final Post post = TestPost.builder()
-                    .id(1L)
-                    .community(community)
-                    .build();
-
-            final Member member = TestMember.builder().id(1L).build();
-
-            given(postRepository.findPostById(anyLong()))
-                    .willReturn(post);
-            given(memberQueryService.getMember(anyLong(), anyLong()))
-                    .willReturn(member);
+            given(postRepository.findPostById(anyLong())).willReturn(post);
+            given(memberQueryService.getMember(anyLong(), anyLong())).willReturn(member);
             given(likeRepository.existsLikeByPostIdAndMemberId(anyLong(), anyLong()))
                     .willReturn(true);
 
@@ -115,48 +115,29 @@ class LikeCommandServiceTest {
     @Nested
     @DisplayName("댓글에 좋아요할 시")
     class DoLikeAtCommentTest {
+
         @Test
         @DisplayName("해당 댓글에 좋아요가 생성된다.")
         void doLikeAtCommentSuccess() {
-            final Community community = TestCommunity.builder().id(1L).build();
-
-            final Post post = TestPost.builder().id(2L).community(community).build();
-
-            final Member member = TestMember.builder().id(3L).community(community).build();
-
-            final Comment comment = TestComment.builder().id(4L).post(post).member(member).build();
-
-            given(commentRepository.findCommentById(anyLong()))
-                    .willReturn(comment);
-            given(memberQueryService.getMember(anyLong(), anyLong()))
-                    .willReturn(member);
+            given(commentRepository.findCommentById(anyLong())).willReturn(comment);
+            given(memberQueryService.getMember(anyLong(), anyLong())).willReturn(member);
             given(likeRepository.existsLikeByCommentIdAndMemberId(anyLong(), anyLong()))
                     .willReturn(false);
 
             likeCommandService.doCommentLike(comment.getId(), 1L);
 
             verify(likeRepository, times(1)).save(likeCaptor.capture());
-            Like newLike = likeCaptor.getValue();
 
-            assertThat(newLike.getComment().getId()).isEqualTo(4L);
+            Like newLike = likeCaptor.getValue();
+            assertThat(newLike.getComment().getId()).isEqualTo(5L);
             assertThat(newLike.getMember().getId()).isEqualTo(3L);
         }
 
         @Test
         @DisplayName("해당 멤버가 해당 댓글에 이미 좋아요를 누른 경우 AlreadyDoCommentLikeException 발생한다.")
         void alreadyDoLikeFail() {
-            final Community community = TestCommunity.builder().id(1L).build();
-
-            final Post post = TestPost.builder().id(2L).community(community).build();
-
-            final Member member = TestMember.builder().id(3L).community(community).build();
-
-            final Comment comment = TestComment.builder().id(4L).post(post).member(member).build();
-
-            given(commentRepository.findCommentById(anyLong()))
-                    .willReturn(comment);
-            given(memberQueryService.getMember(anyLong(), anyLong()))
-                    .willReturn(member);
+            given(commentRepository.findCommentById(anyLong())).willReturn(comment);
+            given(memberQueryService.getMember(anyLong(), anyLong())).willReturn(member);
             given(likeRepository.existsLikeByCommentIdAndMemberId(anyLong(), anyLong()))
                     .willReturn(true);
 
@@ -171,22 +152,10 @@ class LikeCommandServiceTest {
         @Test
         @DisplayName("댓글에 한 좋아요가 삭제된다.")
         void doUnlikeAtCommentSuccess() {
-            final User user = TestUser.builder().id(1L).build();
+            Like like = Like.ofComment(comment, member);
+            ReflectionTestUtils.setField(like, "id", 6L);
 
-            final Member member = TestMember.builder()
-                    .id(1L)
-                    .user(user)
-                    .build();
-
-            final Comment comment = TestComment.builder().id(1L).build();
-
-            final Like like = TestLike.builder()
-                    .id(1L)
-                    .comment(comment)
-                    .member(member)
-                    .build();
-            given(likeRepository.findLikeById(anyLong()))
-                    .willReturn(like);
+            given(likeRepository.findLikeById(anyLong())).willReturn(like);
 
             likeCommandService.doUnlike(like.getId(), 1L);
 
@@ -196,52 +165,24 @@ class LikeCommandServiceTest {
         @Test
         @DisplayName("해당 글의 좋아요 수가 1 감소하고, 글에 한 좋아요가 삭제된다.")
         void doUnlikeAtPostSuccess() {
-            final User user = TestUser.builder().id(1L).build();
+            Like like = Like.ofPost(post, member);
+            ReflectionTestUtils.setField(like, "id", 6L);
 
-            final Member member = TestMember.builder()
-                    .id(1L)
-                    .user(user)
-                    .build();
-
-            final Post post = TestPost.builder()
-                    .id(1L)
-                    .member(member)
-                    .likeCount(1)
-                    .build();
-
-            final Like like = TestLike.builder()
-                    .id(1L)
-                    .post(post)
-                    .member(member)
-                    .build();
-            given(likeRepository.findLikeById(anyLong()))
-                    .willReturn(like);
+            given(likeRepository.findLikeById(anyLong())).willReturn(like);
 
             likeCommandService.doUnlike(like.getId(), 1L);
 
-            assertThat(post.getLikeCount()).isZero();
             verify(likeRepository, times(1)).delete(any(Like.class));
+            assertThat(post.getLikeCount()).isOne();
         }
 
         @Test
         @DisplayName("요청한 세션 유저와 좋아요 한 유저가 서로 다를 경우 UnmatchedLikeUserException 발생한다.")
         void requestedUserAndLikedUserNotSameFail() {
-            final User user = TestUser.builder().id(1L).build();
+            Like like = Like.ofComment(comment, member);
+            ReflectionTestUtils.setField(like, "id", 6L);
 
-            final Member member = TestMember.builder()
-                    .id(1L)
-                    .user(user)
-                    .build();
-
-            final Comment comment = TestComment.builder().id(1L).build();
-
-            final Like like = TestLike.builder()
-                    .id(1L)
-                    .comment(comment)
-                    .member(member)
-                    .build();
-            given(likeRepository.findLikeById(anyLong()))
-                    .willReturn(like);
+            given(likeRepository.findLikeById(anyLong())).willReturn(like);
 
             assertThatThrownBy(() -> likeCommandService.doUnlike(like.getId(), 2L))
                     .isInstanceOf(UnmatchedLikeUserException.class);
--- src/test/java/boogi/apiserver/domain/like/application/LikeQueryServiceTest.java
+++ src/test/java/boogi/apiserver/domain/like/application/LikeQueryServiceTest.java
@@ -1,21 +1,21 @@
 package boogi.apiserver.domain.like.application;
 
-import boogi.apiserver.builder.*;
-import boogi.apiserver.domain.comment.repository.CommentRepository;
 import boogi.apiserver.domain.comment.domain.Comment;
+import boogi.apiserver.domain.comment.repository.CommentRepository;
 import boogi.apiserver.domain.community.community.domain.Community;
-import boogi.apiserver.domain.like.repository.LikeRepository;
 import boogi.apiserver.domain.like.domain.Like;
 import boogi.apiserver.domain.like.dto.response.LikeMembersAtCommentResponse;
 import boogi.apiserver.domain.like.dto.response.LikeMembersAtPostResponse;
+import boogi.apiserver.domain.like.repository.LikeRepository;
 import boogi.apiserver.domain.member.application.MemberQueryService;
 import boogi.apiserver.domain.member.domain.Member;
-import boogi.apiserver.domain.post.post.repository.PostRepository;
 import boogi.apiserver.domain.post.post.domain.Post;
-import boogi.apiserver.domain.user.repository.UserRepository;
+import boogi.apiserver.domain.post.post.repository.PostRepository;
 import boogi.apiserver.domain.user.domain.User;
+import boogi.apiserver.domain.user.repository.UserRepository;
 import boogi.apiserver.global.dto.PaginationDto;
 import boogi.apiserver.global.util.PageableUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -26,11 +26,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static boogi.apiserver.utils.fixture.CommentFixture.COMMENT1;
+import static boogi.apiserver.utils.fixture.CommunityFixture.POCS;
+import static boogi.apiserver.utils.fixture.MemberFixture.SUNDO_POCS;
+import static boogi.apiserver.utils.fixture.PostFixture.POST1;
+import static boogi.apiserver.utils.fixture.UserFixture.SUNDO;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
@@ -57,30 +63,41 @@ class LikeQueryServiceTest {
     @Mock
     CommentRepository commentRepository;
 
+    private User user;
+    private Community community;
+    private Member member;
+    private Post post;
+    private Comment comment;
+
+    @BeforeEach
+    void init() {
+        user = SUNDO.toUser(1L);
+        community = POCS.toCommunity(2L, List.of());
+        member = SUNDO_POCS.toMember(3L, user, community);
+        post = POST1.toPost(4L, member, community, List.of(), List.of());
+        comment = COMMENT1.toComment(5L, post, member, null);
+    }
+
     @Nested
     @DisplayName("글에 한 좋아요의 Id를 가져올시")
     class GetPostLikeIdTest {
         @DisplayName("좋아요가 있을시 해당 좋아요의 Id를 추출해서 가져온다.")
         @Test
         void existsPostLike() {
-            Post post = TestPost.builder().id(1L).build();
-            Member member = TestMember.builder().id(2L).build();
+            Like like = Like.ofPost(post, member);
+            ReflectionTestUtils.setField(like, "id", 6L);
 
-            Like like = TestLike.builder().id(3L).post(post).member(member).build();
             given(likeRepository.findPostLikeByPostAndMember(any(Post.class), any(Member.class)))
                     .willReturn(Optional.of(like));
 
             Long postLikeId = likeQueryService.getPostLikeId(post, member);
 
-            assertThat(postLikeId).isEqualTo(3L);
+            assertThat(postLikeId).isEqualTo(6L);
         }
 
         @DisplayName("좋아요가 없을시 null을 가져온다.")
         @Test
         void notExistsPostLike() {
-            Post post = TestPost.builder().id(1L).build();
-            Member member = TestMember.builder().id(2L).build();
-
             given(likeRepository.findPostLikeByPostAndMember(any(Post.class), any(Member.class)))
                     .willReturn(Optional.empty());
 
@@ -93,29 +110,10 @@ class LikeQueryServiceTest {
     @Test
     @DisplayName("글에 좋아요한 유저들 조회시 유저들을 페이지네이션해서 가져온다.")
     void getLikeMembersAtPostSuccess() {
-        final User user = TestUser.builder().id(1L).build();
+        Like like = Like.ofPost(post, member);
+        ReflectionTestUtils.setField(like, "id", 6L);
 
-        final Community community = TestCommunity.builder().id(2L).build();
-
-        final Member member = TestMember.builder()
-                .id(3L)
-                .user(user)
-                .community(community)
-                .build();
-
-        final Post post = TestPost.builder()
-                .id(4L)
-                .community(community)
-                .build();
-
-        final Like like = TestLike.builder()
-                .id(5L)
-                .post(post)
-                .member(member)
-                .build();
-
-        given(postRepository.findPostById(anyLong()))
-                .willReturn(post);
+        given(postRepository.findPostById(anyLong())).willReturn(post);
 
         Pageable pageable = PageRequest.of(0, 1);
         List<Like> likes = List.of(like);
@@ -124,8 +122,7 @@ class LikeQueryServiceTest {
                 .willReturn(likePage);
 
         List<User> users = List.of(user);
-        given(userRepository.findUsersByIds(anyList()))
-                .willReturn(users);
+        given(userRepository.findUsersByIds(anyList())).willReturn(users);
 
         LikeMembersAtPostResponse response =
                 likeQueryService.getLikeMembersAtPost(post.getId(), 2L, pageable);
@@ -134,7 +131,7 @@ class LikeQueryServiceTest {
                 .getViewableMember(anyLong(), any(Community.class));
 
         PaginationDto pageInfo = response.getPageInfo();
-        assertThat(response.getMembers().size()).isEqualTo(1);
+        assertThat(response.getMembers()).hasSize(1);
         assertThat(response.getMembers().get(0).getId()).isEqualTo(1L);
         assertThat(pageInfo.getNextPage()).isEqualTo(1);
         assertThat(pageInfo.isHasNext()).isFalse();
@@ -143,34 +140,10 @@ class LikeQueryServiceTest {
     @Test
     @DisplayName("댓글에 좋아요한 유저들 조회시 유저들을 페이지네이션해서 가져온다.")
     void getLikeMembersAtCommentSuccess() {
-        final User user = TestUser.builder().id(1L).build();
+        Like like = Like.ofComment(comment, member);
+        ReflectionTestUtils.setField(like, "id", 6L);
 
-        final Community community = TestCommunity.builder().id(2L).build();
-
-        final Member member = TestMember.builder()
-                .id(3L)
-                .user(user)
-                .community(community)
-                .build();
-
-        final Post post = TestPost.builder()
-                .id(4L)
-                .community(community)
-                .build();
-
-        final Comment comment = TestComment.builder()
-                .id(5L)
-                .post(post)
-                .member(member)
-                .build();
-
-        final Like like = TestLike.builder()
-                .id(6L)
-                .comment(comment)
-                .member(member)
-                .build();
-        given(commentRepository.findCommentById(anyLong()))
-                .willReturn(comment);
+        given(commentRepository.findCommentById(anyLong())).willReturn(comment);
 
         Pageable pageable = PageRequest.of(0, 1);
         List<Like> likes = List.of(like);
@@ -179,8 +152,7 @@ class LikeQueryServiceTest {
                 .willReturn(likePage);
 
         List<User> users = List.of(user);
-        given(userRepository.findUsersByIds(anyList()))
-                .willReturn(users);
+        given(userRepository.findUsersByIds(anyList())).willReturn(users);
 
         LikeMembersAtCommentResponse response =
                 likeQueryService.getLikeMembersAtComment(comment.getId(), 2L, pageable);
@@ -189,7 +161,7 @@ class LikeQueryServiceTest {
                 .getViewableMember(anyLong(), any(Community.class));
 
         PaginationDto pageInfo = response.getPageInfo();
-        assertThat(response.getMembers().size()).isEqualTo(1);
+        assertThat(response.getMembers()).hasSize(1);
         assertThat(response.getMembers().get(0).getId()).isEqualTo(1L);
         assertThat(pageInfo.getNextPage()).isEqualTo(1);
         assertThat(pageInfo.isHasNext()).isFalse();
