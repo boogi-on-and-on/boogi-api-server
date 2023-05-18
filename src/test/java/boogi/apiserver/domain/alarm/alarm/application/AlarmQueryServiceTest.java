088c8e93169b558387ef8ed0ace5aacd85c77aca
sdcodebase <sdcodebase@gmail.com>
2023-05-18 10:57:09 +0900

    test: Alarm에 Fixture 적용

--- src/test/java/boogi/apiserver/domain/alarm/alarm/application/AlarmQueryServiceTest.java
+++ src/test/java/boogi/apiserver/domain/alarm/alarm/application/AlarmQueryServiceTest.java
@@ -1,12 +1,12 @@
 package boogi.apiserver.domain.alarm.alarm.application;
 
 
-import boogi.apiserver.builder.TestAlarm;
-import boogi.apiserver.domain.alarm.alarm.repository.AlarmRepository;
 import boogi.apiserver.domain.alarm.alarm.domain.Alarm;
 import boogi.apiserver.domain.alarm.alarm.dto.dto.AlarmsDto;
 import boogi.apiserver.domain.alarm.alarm.dto.response.AlarmsResponse;
-import boogi.apiserver.utils.TestTimeReflection;
+import boogi.apiserver.domain.alarm.alarm.repository.AlarmRepository;
+import boogi.apiserver.domain.user.domain.User;
+import boogi.apiserver.utils.fixture.UserFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,9 +14,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
+import static boogi.apiserver.utils.fixture.AlarmFixture.ALARM1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -34,29 +34,24 @@ class AlarmQueryServiceTest {
     @DisplayName("알림 목록 조회")
     void getAlarms() {
         //given
-
-        final Alarm alarm = TestAlarm.builder()
-                .id(1L)
-                .head("해드1")
-                .body("바디1")
-                .build();
-        TestTimeReflection.setCreatedAt(alarm, LocalDateTime.now());
+        User user = UserFixture.YONGJIN.toUser(1L);
+        Alarm alarm = ALARM1.toAlarm(1L, user);
 
         given(alarmRepository.getAlarms(anyLong()))
                 .willReturn(List.of(alarm));
 
         //when
-        AlarmsResponse alarmsResponse = alarmQueryService.getAlarms(1L);
+        AlarmsResponse alarmsResponse = alarmQueryService.getAlarms(user.getId());
 
         //then
         List<AlarmsDto> dtos = alarmsResponse.getAlarms();
-        
-        assertThat(dtos.size()).isEqualTo(1);
 
-        AlarmsDto first = dtos.get(0);
-        assertThat(first.getBody()).isEqualTo("바디1");
-        assertThat(first.getHead()).isEqualTo("해드1");
-        assertThat(first.getId()).isEqualTo(1L);
-        assertThat(first.getCreatedAt()).isEqualTo(alarm.getCreatedAt().toString());
+        assertThat(dtos).hasSize(1);
+
+        AlarmsDto alarmDto = dtos.get(0);
+        assertThat(alarmDto.getHead()).isEqualTo(ALARM1.head);
+        assertThat(alarmDto.getBody()).isEqualTo(ALARM1.body);
+        assertThat(alarmDto.getId()).isEqualTo(alarm.getId());
+        assertThat(alarmDto.getCreatedAt()).isEqualTo(ALARM1.createdAt.toString());
     }
 }
--- src/test/java/boogi/apiserver/domain/alarm/alarm/repository/AlarmRepositoryTest.java
+++ src/test/java/boogi/apiserver/domain/alarm/alarm/repository/AlarmRepositoryTest.java
@@ -1,25 +1,23 @@
 package boogi.apiserver.domain.alarm.alarm.repository;
 
-import boogi.apiserver.builder.TestAlarm;
-import boogi.apiserver.builder.TestUser;
 import boogi.apiserver.domain.alarm.alarm.domain.Alarm;
 import boogi.apiserver.domain.alarm.alarm.exception.AlarmNotFoundException;
-import boogi.apiserver.domain.user.repository.UserRepository;
 import boogi.apiserver.domain.user.domain.User;
+import boogi.apiserver.domain.user.repository.UserRepository;
 import boogi.apiserver.utils.RepositoryTest;
-import boogi.apiserver.utils.TestTimeReflection;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
+import static boogi.apiserver.utils.fixture.AlarmFixture.*;
+import static boogi.apiserver.utils.fixture.UserFixture.SUNDO;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.groups.Tuple.tuple;
 
 class AlarmRepositoryTest extends RepositoryTest {
 
@@ -29,6 +27,19 @@ class AlarmRepositoryTest extends RepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    private Alarm alarm1;
+    private Alarm alarm2;
+    private Alarm alarm3;
+    private User user;
+
+    @BeforeEach
+    public void init() {
+        this.user = SUNDO.toUser();
+        this.alarm1 = ALARM1.toAlarm(this.user);
+        this.alarm2 = ALARM2.toAlarm(this.user);
+        this.alarm3 = ALARM3.toAlarm(this.user);
+    }
+
     @Nested
     @DisplayName("알람 ID로 알람 조회")
     class findAlarmById {
@@ -36,19 +47,23 @@ class AlarmRepositoryTest extends RepositoryTest {
         @DisplayName("성공")
         @Test
         void success() {
-            final Alarm alarm = TestAlarm.builder().build();
-            alarmRepository.save(alarm);
+            //given
+            userRepository.save(user);
+            alarmRepository.save(alarm1);
 
-            cleanPersistenceContext();
+            //when
+            final Alarm findAlarm = alarmRepository.findAlarmById(alarm1.getId());
 
-            final Alarm findAlarm = alarmRepository.findAlarmById(alarm.getId());
-            assertThat(findAlarm.getId()).isEqualTo(alarm.getId());
+            //then
+            assertThat(findAlarm.getId()).isEqualTo(alarm1.getId());
         }
 
         @DisplayName("throw AlarmNotFoundException")
         @Test
         void throwException() {
+            //then
             assertThatThrownBy(() -> {
+                //when
                 alarmRepository.findAlarmById(1L);
             }).isInstanceOf(AlarmNotFoundException.class);
         }
@@ -57,33 +72,20 @@ class AlarmRepositoryTest extends RepositoryTest {
     @Test
     @DisplayName("유저 ID로 알람을 최신순으로 조회한다.")
     void getAlarms() {
-        final String ALARM_HEAD = "헤드";
-        final String ALARM_BODY = "바디";
-
         //given
-        final User user = TestUser.builder().build();
         userRepository.save(user);
-
-        List<Alarm> alarms = IntStream.range(0, 3)
-                .mapToObj(i -> {
-                    Alarm alarm = TestAlarm.builder()
-                            .head(ALARM_HEAD + i)
-                            .body(ALARM_BODY + i)
-                            .user(user)
-                            .build();
-                    TestTimeReflection.setCreatedAt(alarm, LocalDateTime.now().minusDays(i));
-                    return alarm;
-                }).collect(Collectors.toList());
-        alarmRepository.saveAll(alarms);
-
-        cleanPersistenceContext();
+        alarmRepository.saveAll(List.of(alarm1, alarm2, alarm3));
 
         //when
         List<Alarm> findAlarms = alarmRepository.getAlarms(user.getId());
 
-        List<Long> expectedAlarmIds = alarms.stream().map(Alarm::getId).collect(Collectors.toList());
-        assertThat(findAlarms).extracting("id").isEqualTo(expectedAlarmIds);
-        assertThat(findAlarms).extracting("head").containsExactly("헤드0", "헤드1", "헤드2");
-        assertThat(findAlarms).extracting("body").containsExactly("바디0", "바디1", "바디2");
+        //then
+        assertThat(findAlarms)
+                .extracting("id", "head", "body")
+                .containsExactly(
+                        tuple(alarm1.getId(), ALARM1.head, ALARM1.body),
+                        tuple(alarm2.getId(), ALARM2.head, ALARM2.body),
+                        tuple(alarm3.getId(), ALARM3.head, ALARM3.body)
+                );
     }
 }
