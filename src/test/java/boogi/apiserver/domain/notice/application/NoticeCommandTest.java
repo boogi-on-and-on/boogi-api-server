d7b187d15a77f961626ac835d8da0d62388b7dd3
sdcodebase <sdcodebase@gmail.com>
2023-05-18 21:48:56 +0900

    test: Notice service Fixture 적용

--- src/test/java/boogi/apiserver/domain/notice/application/NoticeCommandTest.java
+++ src/test/java/boogi/apiserver/domain/notice/application/NoticeCommandTest.java
@@ -1,13 +1,13 @@
 package boogi.apiserver.domain.notice.application;
 
 
-import boogi.apiserver.builder.TestCommunity;
-import boogi.apiserver.domain.community.community.repository.CommunityRepository;
 import boogi.apiserver.domain.community.community.domain.Community;
+import boogi.apiserver.domain.community.community.repository.CommunityRepository;
 import boogi.apiserver.domain.member.application.MemberQuery;
-import boogi.apiserver.domain.notice.repository.NoticeRepository;
 import boogi.apiserver.domain.notice.domain.Notice;
 import boogi.apiserver.domain.notice.dto.request.NoticeCreateRequest;
+import boogi.apiserver.domain.notice.repository.NoticeRepository;
+import boogi.apiserver.utils.fixture.CommunityFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,8 +15,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -42,16 +40,17 @@ class NoticeCommandTest {
     @Test
     @DisplayName("생성 성공")
     void success() {
-        final Community community = TestCommunity.builder().id(1L).build();
+        final Long userId = 1L;
+        final Community community = CommunityFixture.POCS.toCommunity(1L, null);
         given(communityRepository.findCommunityById(anyLong()))
                 .willReturn(community);
 
-        NoticeCreateRequest request = new NoticeCreateRequest(1L, "A".repeat(10), "B".repeat(10));
+        NoticeCreateRequest request = new NoticeCreateRequest(community.getId(), "A".repeat(10), "B".repeat(10));
 
-        Long noticeId = noticeCommand.createNotice(request, 2L);
+        Long noticeId = noticeCommand.createNotice(request, userId);
 
         then(memberQuery).should(times(1))
-                .getOperator(2L, 1L);
+                .getOperator(userId, community.getId());
 
         verify(noticeRepository).save(any(Notice.class));
     }
--- src/test/java/boogi/apiserver/domain/notice/application/NoticeQueryTest.java
+++ src/test/java/boogi/apiserver/domain/notice/application/NoticeQueryTest.java
@@ -1,18 +1,19 @@
 package boogi.apiserver.domain.notice.application;
 
-import boogi.apiserver.builder.TestMember;
-import boogi.apiserver.builder.TestNotice;
-import boogi.apiserver.builder.TestUser;
+import boogi.apiserver.domain.community.community.domain.Community;
 import boogi.apiserver.domain.community.community.repository.CommunityRepository;
 import boogi.apiserver.domain.member.application.MemberQuery;
 import boogi.apiserver.domain.member.domain.Member;
-import boogi.apiserver.domain.member.domain.MemberType;
-import boogi.apiserver.domain.notice.repository.NoticeRepository;
 import boogi.apiserver.domain.notice.domain.Notice;
 import boogi.apiserver.domain.notice.dto.dto.NoticeDetailDto;
 import boogi.apiserver.domain.notice.dto.dto.NoticeDto;
 import boogi.apiserver.domain.notice.dto.response.NoticeDetailResponse;
-import boogi.apiserver.utils.TestTimeReflection;
+import boogi.apiserver.domain.notice.repository.NoticeRepository;
+import boogi.apiserver.domain.user.domain.User;
+import boogi.apiserver.utils.fixture.CommunityFixture;
+import boogi.apiserver.utils.fixture.MemberFixture;
+import boogi.apiserver.utils.fixture.UserFixture;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,9 +21,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
+import static boogi.apiserver.utils.fixture.NoticeFixture.NOTICE1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -45,17 +46,20 @@ class NoticeQueryTest {
     @Mock
     MemberQuery memberQuery;
 
+    private Notice notice;
+
+    @BeforeEach
+    public void init() {
+        final Community community = CommunityFixture.POCS.toCommunity(1L, null);
+        final User user = UserFixture.SUNDO.toUser(2L);
+        final Member member = MemberFixture.SUNDO_POCS.toMember(3L, user, community);
+        this.notice = NOTICE1.toNotice(4L, community, member);
+    }
 
     @Test
     @DisplayName("앱 최근공지 조회")
     void lastCreatedNotice() {
         //given
-        final Notice notice = TestNotice.builder()
-                .id(1L)
-                .title("공지사항의 제목입니다.")
-                .build();
-        TestTimeReflection.setCreatedAt(notice, LocalDateTime.now());
-
         given(noticeRepository.getLatestAppNotice())
                 .willReturn(List.of(notice));
 
@@ -64,22 +68,15 @@ class NoticeQueryTest {
 
         //then
         NoticeDto dto = noticeDtos.get(0);
-        assertThat(dto.getId()).isEqualTo(1L);
-        assertThat(dto.getTitle()).isEqualTo("공지사항의 제목입니다.");
-        assertThat(dto.getCreatedAt()).isEqualTo(notice.getCreatedAt().toString());
+        assertThat(dto.getId()).isEqualTo(notice.getId());
+        assertThat(dto.getTitle()).isEqualTo(NOTICE1.title);
+        assertThat(dto.getCreatedAt()).isEqualTo(NOTICE1.createdAt.toString());
     }
 
     @Test
     @DisplayName("전체 앱 공지사항 조회")
     void allNotice() {
         //given
-        final Notice notice = TestNotice.builder()
-                .id(1L)
-                .title("공지사항의 제목입니다.")
-                .content("공지사항의 내용입니다.")
-                .build();
-        TestTimeReflection.setCreatedAt(notice, LocalDateTime.now());
-
         given(noticeRepository.getAllAppNotices())
                 .willReturn(List.of(notice));
 
@@ -87,72 +84,62 @@ class NoticeQueryTest {
         final NoticeDetailResponse response = noticeQuery.getAppNotice();
 
         //then
-
         assertThat(response.getManager()).isNull();
 
         final NoticeDetailDto dto = response.getNotices().get(0);
-        assertThat(dto.getId()).isEqualTo(1L);
-        assertThat(dto.getTitle()).isEqualTo("공지사항의 제목입니다.");
-        assertThat(dto.getContent()).isEqualTo("공지사항의 내용입니다.");
-        assertThat(dto.getCreatedAt()).isEqualTo(notice.getCreatedAt().toString());
+        assertThat(dto.getId()).isEqualTo(notice.getId());
+        assertThat(dto.getTitle()).isEqualTo(NOTICE1.title);
+        assertThat(dto.getContent()).isEqualTo(NOTICE1.content);
+        assertThat(dto.getCreatedAt()).isEqualTo(NOTICE1.createdAt.toString());
     }
 
     @Test
     @DisplayName("커뮤니티 최근 공지사항 목록 조회")
     void latestCommunityNotice() {
-        final Notice notice = TestNotice.builder()
-                .id(1L)
-                .title("공지사항 제목입니다.")
-                .build();
-        TestTimeReflection.setCreatedAt(notice, LocalDateTime.now());
-
+        //given
         given(noticeRepository.getLatestNotice(anyLong()))
                 .willReturn(List.of(notice));
 
-        final List<NoticeDto> dtos = noticeQuery.getCommunityLatestNotice(1L);
+        final Long communityId = notice.getCommunity().getId();
+        //when
+        final List<NoticeDto> dtos = noticeQuery.getCommunityLatestNotice(communityId);
 
+        //then
         verify(communityRepository, times(1)).findCommunityById(anyLong());
 
         assertThat(dtos.size()).isEqualTo(1);
 
         final NoticeDto dto = dtos.get(0);
-        assertThat(dto.getId()).isEqualTo(1L);
-        assertThat(dto.getTitle()).isEqualTo("공지사항 제목입니다.");
-        assertThat(dto.getCreatedAt()).isEqualTo(notice.getCreatedAt().toString());
+        assertThat(dto.getId()).isEqualTo(notice.getId());
+        assertThat(dto.getTitle()).isEqualTo(NOTICE1.title);
+        assertThat(dto.getCreatedAt()).isEqualTo(NOTICE1.createdAt.toString());
     }
 
     @Test
     @DisplayName("커뮤니티 전체 공지사항 목록 조회")
     void allCommunityNotice() {
-        final Member member = TestMember.builder()
-                .user(TestUser.builder().build())
-                .memberType(MemberType.MANAGER)
-                .build();
+        //given
+        Long userId = notice.getMember().getUser().getId();
+        Long communityId = notice.getCommunity().getId();
 
         given(memberQuery.getMember(any(), anyLong()))
-                .willReturn(member);
-
-        final Notice notice = TestNotice.builder()
-                .id(1L)
-                .member(member)
-                .title("공지사항 제목입니다.")
-                .build();
-        TestTimeReflection.setCreatedAt(notice, LocalDateTime.now());
+                .willReturn(notice.getMember());
 
         given(noticeRepository.getAllNotices(anyLong()))
                 .willReturn(List.of(notice));
 
-        final NoticeDetailResponse response = noticeQuery.getCommunityNotice(1L, 1L);
+        //when
+        final NoticeDetailResponse response = noticeQuery.getCommunityNotice(userId, communityId);
 
         verify(communityRepository, times(1)).findCommunityById(anyLong());
 
         final List<? extends NoticeDetailDto> dtos = response.getNotices();
         assertThat(response.getManager()).isTrue();
-        assertThat(dtos.size()).isEqualTo(1);
+        assertThat(dtos).hasSize(1);
 
         final NoticeDto dto = dtos.get(0);
-        assertThat(dto.getId()).isEqualTo(1L);
-        assertThat(dto.getTitle()).isEqualTo("공지사항 제목입니다.");
-        assertThat(dto.getCreatedAt()).isEqualTo(notice.getCreatedAt().toString());
+        assertThat(dto.getId()).isEqualTo(notice.getId());
+        assertThat(dto.getTitle()).isEqualTo(NOTICE1.title);
+        assertThat(dto.getCreatedAt()).isEqualTo(NOTICE1.createdAt.toString());
     }
 }
