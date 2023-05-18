afc99b315b33fe95287dbf16c5eda4402cf4a272
yjlee0235 <yjlee0235@naver.com>
2023-05-18 09:30:02 +0900

    Test: MessageBlock ServiceTest에 Fixture 적용

--- src/test/java/boogi/apiserver/domain/message/block/application/MessageBlockCommandServiceTest.java
+++ src/test/java/boogi/apiserver/domain/message/block/application/MessageBlockCommandServiceTest.java
@@ -1,13 +1,11 @@
 package boogi.apiserver.domain.message.block.application;
 
 
-import boogi.apiserver.builder.TestMessageBlock;
-import boogi.apiserver.builder.TestUser;
-import boogi.apiserver.domain.message.block.repository.MessageBlockRepository;
 import boogi.apiserver.domain.message.block.domain.MessageBlock;
 import boogi.apiserver.domain.message.block.exception.NotBlockedUserException;
-import boogi.apiserver.domain.user.repository.UserRepository;
+import boogi.apiserver.domain.message.block.repository.MessageBlockRepository;
 import boogi.apiserver.domain.user.domain.User;
+import boogi.apiserver.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -21,6 +19,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 import java.util.Optional;
 
+import static boogi.apiserver.utils.fixture.UserFixture.SUNDO;
+import static boogi.apiserver.utils.fixture.UserFixture.YONGJIN;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyList;
@@ -40,7 +40,6 @@ class MessageBlockCommandServiceTest {
     @Mock
     UserRepository userRepository;
 
-
     @Nested
     @DisplayName("쪽지 차단을 해제할시")
     class UnblockUserTest {
@@ -48,7 +47,7 @@ class MessageBlockCommandServiceTest {
         @DisplayName("차단되지 않은 유저에게 차단 해제시 NotBlockedUserException 예외가 발생한다.")
         void unblockToNotBlockedUserFail() {
             //given
-            final MessageBlock block = TestMessageBlock.builder().blocked(false).build();
+            MessageBlock block = MessageBlock.builder().blocked(false).build();
             given(messageBlockRepository.getMessageBlockByUserId(anyLong(), anyLong()))
                     .willReturn(Optional.of(block));
 
@@ -61,7 +60,7 @@ class MessageBlockCommandServiceTest {
         @DisplayName("쪽지 차단이 해제된다.")
         void unblockMemberMessage() {
             //given
-            final MessageBlock block = TestMessageBlock.builder().blocked(true).build();
+            MessageBlock block = MessageBlock.builder().blocked(true).build();
             given(messageBlockRepository.getMessageBlockByUserId(anyLong(), anyLong()))
                     .willReturn(Optional.of(block));
 
@@ -76,7 +75,6 @@ class MessageBlockCommandServiceTest {
     @Nested
     @DisplayName("쪽지 차단할시")
     class BlockUserTest {
-
         @Captor
         ArgumentCaptor<List<MessageBlock>> messageBlocksCaptor;
 
@@ -84,23 +82,20 @@ class MessageBlockCommandServiceTest {
         @DisplayName("이미 쪽지 차단 엔티티가 존재하는 경우 해당 엔티티를 업데이트하고, 존재하지 않으면 새로 생성한다.")
         void blockeUsersSuccess() {
             //given
-            final User user = TestUser.builder().id(1L).build();
-            given(userRepository.findUserById(anyLong()))
-                    .willReturn(user);
+            User user = SUNDO.toUser(1L);
+            User blockedUser = YONGJIN.toUser(2L);
 
-            final User blockedUser = TestUser.builder().id(2L).build();
-
-            final MessageBlock block = TestMessageBlock.builder()
+            final MessageBlock block = MessageBlock.builder()
                     .id(3L)
                     .user(user)
                     .blockedUser(blockedUser)
                     .blocked(false)
                     .build();
+
+            given(userRepository.findUserById(anyLong())).willReturn(user);
             given(messageBlockRepository.getMessageBlocksByUserIds(anyLong(), anyList()))
                     .willReturn(List.of(block));
-
-            given(userRepository.findUsersByIds(anyList()))
-                    .willReturn(List.of(blockedUser));
+            given(userRepository.findUsersByIds(anyList())).willReturn(List.of(blockedUser));
 
             //when
             messageBlockCommandService.blockUsers(1L, List.of(blockedUser.getId()));
@@ -108,11 +103,13 @@ class MessageBlockCommandServiceTest {
             //then
             verify(messageBlockRepository, times(1))
                     .updateBulkBlockedStatus(anyLong(), anyList());
-            verify(messageBlockRepository, times(1)).saveAll(messageBlocksCaptor.capture());
+            verify(messageBlockRepository, times(1))
+                    .saveAll(messageBlocksCaptor.capture());
 
             List<MessageBlock> messageBlocks = messageBlocksCaptor.getValue();
 
-            assertThat(messageBlocks.size()).isEqualTo(1);
+            assertThat(messageBlocks).hasSize(1);
+
             MessageBlock newMessageBlock = messageBlocks.get(0);
             assertThat(newMessageBlock.getUser().getId()).isEqualTo(1L);
             assertThat(newMessageBlock.getBlockedUser().getId()).isEqualTo(2L);
